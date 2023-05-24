a4fc0648d448fa09449483981d45cc8fb47b8352
Zorkador <bielduartes44@gmail.com>
2023-05-24 18:03:28 -0300

    Updates: Create the game page. Create local storage for the name of the player. Create functions, add images and animation for the cards. Fix the menu play btn refresh bug

---
+++ js/games.js
@@ -0,0 +1,48 @@
+const grid = document.querySelector(".grid");
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+]
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const createCard = (character) => {
+
+    const card = createElement("div", "card");
+    const front = createElement("div", "face front");
+    const back = createElement("div", "face back");
+
+    front.style.backgroundImage = `url("../images/${character}.png")`;
+
+    //appendChild used to insert the div's into the mother div
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+}
+
+const loadGame = () => {
+
+    characters.forEach((character) => {
+
+        const card = createCard(character);
+        grid.appendChild(card);
+
+    });
+
+}
+
+loadGame();
---
+++ js/login.js
@@ -0,0 +1,29 @@
+const input = document.querySelector(".login__input");
+const button = document.querySelector(".login__button");
+const form = document.querySelector(".login-form");
+
+input.value = "";
+button.setAttribute("disabled", "");
+
+const validateInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute("disabled");
+        return;
+    }
+
+    button.setAttribute("disabled", "");
+}
+
+const handleSubmit = (event) => {
+    //Prevent refresh of the pag
+    event.preventDefault();
+    
+    localStorage.setItem("Player", input.value);
+
+    window.location = "pages/game.html";
+} 
+
+
+
+input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
